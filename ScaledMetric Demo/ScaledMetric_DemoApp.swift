3a477905500f007af18c4c5305ec62bd531de581
Brett-Best <brettbest@live.com.au>
2024-03-17 20:00:54 +1100

    Initial Commit

---
+++ ScaledMetric Demo/ScaledMetric_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaledMetric_DemoApp.swift
+//  ScaledMetric Demo
+//
+//  Created by Brett Best on 17/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScaledMetric_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
